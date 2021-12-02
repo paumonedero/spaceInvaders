31a884a639e88e3f50e64be4c6cd79f2f518ba34
Pau Monedero Roigé <pmonederor2021@cepnet.net>
2021-12-02 11:16:49 +0100

    finished the menu

--- js/script.js
+++ js/script.js
@@ -1,298 +1,294 @@
 let botonJugar = document.getElementById("jugar");
 
+botonJugar.addEventListener("click", function () {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("juego").style.display = "flex";
+  //Declaración de variables.
+  const grid = document.querySelector(".grid");
+  let posicionNave = 202;
+  let width = 15;
+  let direccion = 1;
+  let irDerecha = true;
+  let displayResultado = document.querySelector(".resultados");
+  let resultat = 0;
+  var numDada = 1;
 
+  let paused = false;
 
+  let alienId;
+  let aliensBorrats = [];
+  let laserId;
+  let posicionEspeciales = [];
 
+  const aliens = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39,
+  ];
 
+  //Generamos el grid de cuadrados
+  for (let i = 0; i < 225; i++) {
+    const quadrados = document.createElement("div");
+    grid.appendChild(quadrados);
+  }
+  const quadrados = Array.from(document.querySelectorAll(".grid div"));
 
-botonJugar.addEventListener("click", function(){
+  //Añadimos la nave en su posición inicial.
+  quadrados[posicionNave].classList.add("nave");
 
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("juego").style.display = "flex";
-//Declaración de variables.
-    const grid = document.querySelector(".grid");
-    let posicionNave = 202;
-    let width = 15;
-    let direccion = 1;
-    let irDerecha = true;
-    let displayResultado = document.querySelector(".resultados");
-    let resultat = 0;
-    var numDada = 1;
-    
-    let paused = false;
-    
-    let alienId;
-    let aliensBorrats = [];
-    let laserId;
-    let posicionEspeciales = [];
-    
-    const aliens = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-    ];
-    
-    //Generamos el grid de cuadrados
-    for (let i = 0; i < 225; i++) {
-        const quadrados = document.createElement("div");
-        grid.appendChild(quadrados);
+  //Los event listener que hacen que al pulsar las teclas podamos mover la nave o disparar
+  document.addEventListener("keydown", moverNave);
+  document.addEventListener("keydown", disparar);
+  document.addEventListener("keydown", function (e) {
+    //Es el event listener que activa la funcio de pausar o la de reanudar el joc.
+    if (e.key == "Escape") {
+      if (paused) {
+        reanudarJuego();
+      } else {
+        pausarJuego();
+      }
     }
-    const quadrados = Array.from(document.querySelectorAll(".grid div"));
-    
-    //Añadimos la nave en su posición inicial.
-    quadrados[posicionNave].classList.add("nave");
-    
-    //Los event listener que hacen que al pulsar las teclas podamos mover la nave o disparar
-    document.addEventListener('keydown', moverNave);
-    document.addEventListener("keydown", disparar);
-    document.addEventListener("keydown", function(e){//Es el event listener que activa la funcio de pausar o la de reanudar el joc.
-        if(e.key == 'Escape'){
-            if(paused){
-                reanudarJuego();
-            }else{
-                pausarJuego();
-            }
-        }
-    });
-    
-    //dibujamos todos los aliens (tanto especiales como normales) por primera vez (antes de dibujar los especiales generamos su posicion).
-    alienId = setInterval(moverAliens, 700);
-    generarPosicionEspeciales();
+  });
+
+  //dibujamos todos los aliens (tanto especiales como normales) por primera vez (antes de dibujar los especiales generamos su posicion).
+  alienId = setInterval(moverAliens, 700);
+  generarPosicionEspeciales();
+  dibujarAliens();
+  let disparoAlien = setInterval(dispararAliens, 1500);
+  let disparoAlien2 = setInterval(dispararAliens, 2000);
+  //dibujarEspeciales();
+
+  //generarPosicionEspeciales --> para guardar la posicion del array en la que estarán los aliens especiales.
+  function generarPosicionEspeciales() {
+    let i = 0;
+    while (i < 3) {
+      //de momento genera 3, despues generarà dependiendo del grado seleccionado.
+      let random1 = Math.floor(Math.random() * aliens.length);
+      if (!posicionEspeciales.includes(random1)) {
+        posicionEspeciales.push(random1);
+        i++;
+      }
+    }
+  }
+
+  //dibujar todos los aliens (llamamos a dibujar los especiales tambien).
+  function dibujarAliens() {
+    for (let i = 0; i < aliens.length; i++) {
+      if (!aliensBorrats.includes(i)) {
+        quadrados[aliens[i]].classList.add("alien");
+      }
+    }
+    for (let i = 0; i < posicionEspeciales.length; i++) {
+      if (!aliensBorrats.includes(posicionEspeciales[i])) {
+        dibujarEspeciales(i); //li passo el valor de "i" perque aixi sap quina es la posició que ha de borrar.
+      }
+    }
+  }
+
+  //dibujar los aliens especiales
+  function dibujarEspeciales(i) {
+    quadrados[aliens[posicionEspeciales[i]]].classList.add("alienEspecial");
+  }
+
+  function borrarAliens() {
+    for (let i = 0; i < aliens.length; i++) {
+      quadrados[aliens[i]].classList.remove("alien", "alienEspecial");
+    }
+  }
+
+  function moverNave(e) {
+    if (paused == false) {
+      quadrados[posicionNave].classList.remove("nave");
+
+      switch (e.key) {
+        case "ArrowLeft":
+          if (posicionNave % width !== 0) {
+            posicionNave = posicionNave - 1;
+          }
+          break;
+
+        case "ArrowRight":
+          if (posicionNave % width < width - 1) {
+            posicionNave = posicionNave + 1;
+          }
+          break;
+      }
+      quadrados[posicionNave].classList.add("nave");
+    }
+  }
+
+  function moverAliens() {
+    const bordeIzquierdo = aliens[0] % width === 0;
+    const bordeDerecho = aliens[aliens.length - 1] % width === width - 1;
+    borrarAliens();
+
+    if (bordeDerecho && irDerecha) {
+      for (let i = 0; i < aliens.length; i++) {
+        aliens[i] = aliens[i] + width + 1;
+        direccion = -1;
+        irDerecha = false;
+      }
+    }
+
+    if (bordeIzquierdo && !irDerecha) {
+      for (let i = 0; i < aliens.length; i++) {
+        aliens[i] = aliens[i] + width - 1;
+        direccion = 1;
+        irDerecha = true;
+      }
+    }
+
+    for (let i = 0; i < aliens.length; i++) {
+      aliens[i] = aliens[i] + direccion;
+    }
+
     dibujarAliens();
-    let disparoAlien = setInterval(dispararAliens, 1500);
-    let disparoAlien2 = setInterval(dispararAliens, 2000);
-    //dibujarEspeciales();
-    
-    //generarPosicionEspeciales --> para guardar la posicion del array en la que estarán los aliens especiales.
-    function generarPosicionEspeciales() {
-        let i=0;
-        while(i<3) { //de momento genera 3, despues generarà dependiendo del grado seleccionado.
-            let random1 = Math.floor(Math.random() * aliens.length);
-            if(!posicionEspeciales.includes(random1)){
-                posicionEspeciales.push(random1);
-                i++;
-            }
-        }
-    }
-    
-    //dibujar todos los aliens (llamamos a dibujar los especiales tambien).
-    function dibujarAliens() {
-        for (let i = 0; i < aliens.length; i++) {
-            if (!aliensBorrats.includes(i)) {
-                quadrados[aliens[i]].classList.add("alien");
-            }
-        }
-        for(let i=0; i<posicionEspeciales.length; i++){
-            if(!aliensBorrats.includes(posicionEspeciales[i])){
-                dibujarEspeciales(i); //li passo el valor de "i" perque aixi sap quina es la posició que ha de borrar.
-            }
-        }
-        
-    }
-    
-    //dibujar los aliens especiales
-    function dibujarEspeciales(i){
-            quadrados[aliens[posicionEspeciales[i]]].classList.add("alienEspecial");   
-    }
-    
-    
-    function borrarAliens() {
-        for (let i = 0; i < aliens.length; i++) {
-            quadrados[aliens[i]].classList.remove("alien", "alienEspecial");
-        }
-    }
-    
-    function moverNave(e) {
-        if(paused == false){
-            quadrados[posicionNave].classList.remove("nave");
-    
-        switch (e.key) {
-            case "ArrowLeft":
-                if (posicionNave % width !== 0) {
-                    posicionNave = posicionNave - 1;
-                }
-                break;
-    
-            case "ArrowRight":
-                if (posicionNave % width < width - 1) {
-                    posicionNave = posicionNave + 1;
-                }
-                break;
-        }
-        quadrados[posicionNave].classList.add("nave");
-        }
-    }
-    
-    function moverAliens() {
-        const bordeIzquierdo = aliens[0] % width === 0;
-        const bordeDerecho = aliens[aliens.length - 1] % width === width - 1;
-        borrarAliens();
-    
-        if (bordeDerecho && irDerecha) {
-            for (let i = 0; i < aliens.length; i++) {
-                aliens[i] = aliens[i] + width + 1;
-                direccion = -1;
-                irDerecha = false;
-            }
-        }
-    
-        if (bordeIzquierdo && !irDerecha) {
-            for (let i = 0; i < aliens.length; i++) {
-                aliens[i] = aliens[i] + width - 1;
-                direccion = 1;
-                irDerecha = true;
-            }
-        }
-    
-        for (let i = 0; i < aliens.length; i++) {
-            aliens[i] = aliens[i] + direccion;
-        }
-    
-        dibujarAliens();
-    
-        if (quadrados[posicionNave].classList.contains("alien", "nave")) {
-            displayResultado.innerHTML = "GAME OVER";
-            clearInterval(alienId);
-        }
-    
-        for (let i = 0; i < aliens.length; i++) {
-            if (aliens[i] > quadrados.length) {
-                displayResultado.innerHTML = "GAME OVER";
-                clearInterval(alienId);
-            }
-        }
-        if (aliensBorrats.length === aliens.length) {
-            displayResultado.innerHTML = "HAS GUANYAT!";
-            clearInterval(alienId);
-            clearInterval(disparoAlien);
-            clearInterval(disparoAlien2);
-        }
+
+    if (quadrados[posicionNave].classList.contains("alien", "nave")) {
+      displayResultado.innerHTML = "GAME OVER";
+      clearInterval(alienId);
     }
 
-    function dispararAliens(){
-        if(paused == false){
-            let random2 = Math.floor(Math.random() * aliens.length); //FALTA ARREGLAR PERQUE NO SURTI EL LASER D'ON JA NO HI HA UN ALIEN PERQUE HA SIGUT DESTRUIT
-            let posicionLaserAlien = aliens[random2];
-
-            function moverLaserAlien(){
-                quadrados[posicionLaserAlien].classList.remove("laserAlien");
-                posicionLaserAlien = posicionLaserAlien + width;
-                quadrados[posicionLaserAlien].classList.add("laserAlien");
-
-                if(quadrados[posicionLaserAlien + width] == undefined){
-                    quadrados[posicionLaserAlien].classList.remove("laserAlien");
-                }
-                if(quadrados[posicionLaserAlien].classList.contains("nave")){
-                    clearInterval(alienId);
-                    paused = true;
-                    Swal.fire({
-                        position: 'top-end',
-                        icon: 'error',
-                        title: "T'han donat a la nau! -15 punts!",
-                        showConfirmButton: false,
-                        timer: 1000
-                      }).then((result) => {
-                        if (result.dismiss === Swal.DismissReason.timer) {
-                          reanudarJuego();
-                        }
-                      })
-                      resultat = resultat - 15;
-                      displayResultado.innerHTML = resultat;
-                }
-            }
-            setInterval(moverLaserAlien, 100);
-        }
-    }
-
-    function disparar(e) {
-        if(paused == false){
-            let posicionLaser = posicionNave;
-    
-        function moverLaser() {
-            quadrados[posicionLaser].classList.remove("laser");
-            posicionLaser = posicionLaser - width;
-            quadrados[posicionLaser].classList.add("laser");
-    
-            if (quadrados[posicionLaser - width] == undefined) {
-                quadrados[posicionLaser].classList.remove("laser");
-                clearInterval(laserId);
-                laserId = undefined;
-            }
-    
-            if (quadrados[posicionLaser].classList.contains("alien")) {
-                quadrados[posicionLaser].classList.remove("laser");
-                quadrados[posicionLaser].classList.remove("alien");
-                if(quadrados[posicionLaser].classList.contains("alienEspecial")){
-                    clearInterval(alienId);
-                    paused = true;
-                    quadrados[posicionLaser].classList.remove("alienEspecial");
-                    Swal.fire({
-                        icon: 'info',
-                        title: 'Sortida laboral '+numDada,
-                        text: 'Text de la base de dades de la sortida laboral',
-                        allowOutsideClick: false,
-                        allowEscapeKey: false
-                      }).then((result) => {
-                        if (result.isConfirmed) {
-                            reanudarJuego();
-                        }
-                      })
-                    
-                      numDada++;
-                      resultat = resultat + 5; 
-                }
-                quadrados[posicionLaser].classList.add("boom");
-    
-                setTimeout(() => quadrados[posicionLaser].classList.remove("boom"), 300);
-                clearInterval(laserId);
-                laserId = undefined;
-    
-                const alienBorrat = aliens.indexOf(posicionLaser);
-                aliensBorrats.push(alienBorrat);
-                resultat= resultat + 5;
-                displayResultado.innerHTML = resultat;
-            }
-        }
-    
-        switch (e.key) {
-            case " ":
-                if (!laserId) laserId = setInterval(moverLaser, 50);
-        }
-        }
-    }
-    
-    function reanudarJuego(){
-        alienId = setInterval(moverAliens, 1000);
-        paused = false;
-    }
-    
-    function pausarJuego(){
+    for (let i = 0; i < aliens.length; i++) {
+      if (aliens[i] > quadrados.length) {
+        displayResultado.innerHTML = "GAME OVER";
         clearInterval(alienId);
-        paused = true;
-        menuPause();
+      }
     }
+    if (aliensBorrats.length === aliens.length) {
+      displayResultado.innerHTML = "HAS GUANYAT!";
+      clearInterval(alienId);
+      clearInterval(disparoAlien);
+      clearInterval(disparoAlien2);
+    }
+  }
 
-    function menuPause(){
-        Swal.fire({
-            customClass:{
-                title: 'title-custom',
-            },
-            title: 'Joc Pausat',
-            width: 600,
-            padding: '3em',
-            background: '#fff url(./img/pauseBackground.jpg) 200px',
-            confirmButtonText: 'Reprendre',
-            allowEscapeKey: false,
-            allowOutsideClick: false,
-            backdrop: `
+  function dispararAliens() {
+    if (paused == false) {
+      let random2 = Math.floor(Math.random() * aliens.length); //FALTA ARREGLAR PERQUE NO SURTI EL LASER D'ON JA NO HI HA UN ALIEN PERQUE HA SIGUT DESTRUIT
+      let posicionLaserAlien = aliens[random2];
+
+      function moverLaserAlien() {
+        quadrados[posicionLaserAlien].classList.remove("laserAlien");
+        posicionLaserAlien = posicionLaserAlien + width;
+        quadrados[posicionLaserAlien].classList.add("laserAlien");
+
+        if (quadrados[posicionLaserAlien + width] == undefined) {
+          quadrados[posicionLaserAlien].classList.remove("laserAlien");
+        }
+        if (quadrados[posicionLaserAlien].classList.contains("nave")) {
+          clearInterval(alienId);
+          paused = true;
+          Swal.fire({
+            position: "top-end",
+            icon: "error",
+            title: "T'han donat a la nau! -15 punts!",
+            showConfirmButton: false,
+            timer: 1000,
+          }).then((result) => {
+            if (result.dismiss === Swal.DismissReason.timer) {
+              reanudarJuego();
+            }
+          });
+          resultat = resultat - 15;
+          displayResultado.innerHTML = resultat;
+        }
+      }
+      setInterval(moverLaserAlien, 100);
+    }
+  }
+
+  function disparar(e) {
+    if (paused == false) {
+      let posicionLaser = posicionNave;
+
+      function moverLaser() {
+        quadrados[posicionLaser].classList.remove("laser");
+        posicionLaser = posicionLaser - width;
+        quadrados[posicionLaser].classList.add("laser");
+
+        if (quadrados[posicionLaser - width] == undefined) {
+          quadrados[posicionLaser].classList.remove("laser");
+          clearInterval(laserId);
+          laserId = undefined;
+        }
+
+        if (quadrados[posicionLaser].classList.contains("alien")) {
+          quadrados[posicionLaser].classList.remove("laser");
+          quadrados[posicionLaser].classList.remove("alien");
+          if (quadrados[posicionLaser].classList.contains("alienEspecial")) {
+            clearInterval(alienId);
+            paused = true;
+            quadrados[posicionLaser].classList.remove("alienEspecial");
+            Swal.fire({
+              icon: "info",
+              title: "Sortida laboral " + numDada,
+              text: "Text de la base de dades de la sortida laboral",
+              allowOutsideClick: false,
+              allowEscapeKey: false,
+            }).then((result) => {
+              if (result.isConfirmed) {
+                reanudarJuego();
+              }
+            });
+
+            numDada++;
+            resultat = resultat + 5;
+          }
+          quadrados[posicionLaser].classList.add("boom");
+
+          setTimeout(
+            () => quadrados[posicionLaser].classList.remove("boom"),
+            300
+          );
+          clearInterval(laserId);
+          laserId = undefined;
+
+          const alienBorrat = aliens.indexOf(posicionLaser);
+          aliensBorrats.push(alienBorrat);
+          resultat = resultat + 5;
+          displayResultado.innerHTML = resultat;
+        }
+      }
+
+      switch (e.key) {
+        case " ":
+          if (!laserId) laserId = setInterval(moverLaser, 50);
+      }
+    }
+  }
+
+  function reanudarJuego() {
+    alienId = setInterval(moverAliens, 1000);
+    paused = false;
+  }
+
+  function pausarJuego() {
+    clearInterval(alienId);
+    paused = true;
+    menuPause();
+  }
+
+  function menuPause() {
+    Swal.fire({
+      customClass: {
+        title: "title-custom",
+      },
+      title: "Joc Pausat",
+      width: 600,
+      padding: "3em",
+      background: "#fff url(./img/pauseBackground.jpg) 200px",
+      confirmButtonText: "Reprendre",
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      backdrop: `
               rgba(0,0,123,0.4)
               url("./img/pauseIcon.png")
               left top
-              no-repeat`
-          }).then((result) => {
-            if (result.isConfirmed) {
-                reanudarJuego();
-            }
-          })
-    }
+              no-repeat`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        reanudarJuego();
+      }
+    });
+  }
 });
